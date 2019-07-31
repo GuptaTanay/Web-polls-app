32222b4d9bf4d71bc80dbdd4e89323252a9cc9fc
GuptaTanay <tanaygupta.gupta@gmail.com>
2019-07-31 22:52:07 +0530

    Second commit

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from django.apps import apps
 # Register your models here.
+
+
+app = apps.get_app_config('polls')
+for model_name, model in list(app.models.items()):
+    admin.site.register(model)
